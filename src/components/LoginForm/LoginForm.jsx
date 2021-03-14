64bfbe5d0324333a16bee394c2474919ad246d9e
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-14 16:41:54 +0100

    Add LoginForm component

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const LoginForm = () => {
+  return (
+    <S.Container>
+      <h1>LoginForm</h1>
+    </S.Container>
+  );
+};
+
+export default LoginForm;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import * as S from "./styled";
+import LoginForm from "../../components/LoginForm";
 
 const LandingPage = () => {
   return (
     <S.Container>
       <h2>LandingPage</h2>
+      <LoginForm />
     </S.Container>
   );
 };
