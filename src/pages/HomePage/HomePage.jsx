794cab01dc5ec6d50d7e0fe22ae6d61e98279602
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-14 16:04:29 +0100

    Create LandingPage and HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const HomePage = () => {
+  return (
+    <S.Container>
+      <h1>HomePage</h1>
+    </S.Container>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const LandingPage = () => {
+  return (
+    <S.Container>
+      <h1>LandingPage</h1>
+    </S.Container>
+  );
+};
+
+export default LandingPage;
