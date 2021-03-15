caa93eaca65d7c560c3020adbc9450c58fdd648e
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-15 11:09:40 +0100

    Styles for plants img

--- src/components/PlantItem/PlantItem.jsx
+++ src/components/PlantItem/PlantItem.jsx
@@ -4,10 +4,9 @@ import * as S from "./styled";
 const PlantItem = ({ title, description, img, id, temperature }) => {
   return (
     <S.Container>
-      <strong>{id}</strong>
+      <S.Image src={img} />
       <h4>{title}</h4>
       <p>{description}</p>
-      <img src={img} />
       <p>{temperature}</p>
     </S.Container>
   );
--- src/components/PlantItem/styled.js
+++ src/components/PlantItem/styled.js
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
