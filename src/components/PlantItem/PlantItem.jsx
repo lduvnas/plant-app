1e6992a746cec199c5c1ee08226cc9ddc6bbf701
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-15 17:41:15 +0100

    Create DetailPage

--- src/components/PlantItem/PlantItem.jsx
+++ src/components/PlantItem/PlantItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import * as S from "./styled";
+import { useHistory } from "react-router-dom";
 
 const PlantItem = ({ title, description, img, id, temperature }) => {
+  const history = useHistory();
   return (
-    <S.Container>
+    <S.Container onClick={() => history.push(`/plant/${id}`)}>
       <S.Image src={img} />
       <h4>{title}</h4>
       <p>{description}</p>
---
+++ src/pages/DetailPage/DetailPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const DetailPage = () => {
+  return (
+    <S.Container>
+      <h1>DetailPage</h1>
+    </S.Container>
+  );
+};
+
+export default DetailPage;
