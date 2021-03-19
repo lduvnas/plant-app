f13e7dd0ca74bb8ccbec867057a3a117cb11fceb
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-19 16:37:40 +0100

    Create fetch function for favorites

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./styled";
 import { useAuth } from "../../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 import PlantList from "../../components/PlantList";
+import { db } from "../../firebase";
 
 const HomePage = () => {
   const [error, setError] = useState();
+  const [favoritesList, setFavoritesList] = useState([]);
   const { currentUser, logout } = useAuth();
   const history = useHistory();
 
@@ -19,11 +21,33 @@ const HomePage = () => {
     }
   }
 
+  useEffect(() => {
+    fetchFavorites();
+  }, []);
+
+  function fetchFavorites() {
+    db.collection("users")
+      .doc(currentUser.uid)
+      .get()
+      .then((doc) => {
+        console.log(doc.data().favorites);
+        setFavoritesList(doc.data().favorites);
+      });
+  }
+
   return (
     <S.Container>
       <h2>HomePage</h2>
       <strong>email: </strong> {currentUser.email}
       <button onClick={handleLogout}>Log out</button>
+      {favoritesList && favoritesList.length !== 0 ? <h3>Your garden:</h3> : ""}
+      {favoritesList.length === 0 ? (
+        <p>Inga plantor än</p>
+      ) : (
+        favoritesList.map((item) => {
+          return <p>{item}</p>;
+        })
+      )}
       <PlantList />
     </S.Container>
   );
