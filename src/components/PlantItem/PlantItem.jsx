bdacecf0b5cfad30c2d785b32241ae19f25929a4
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-19 21:29:10 +0100

    Add "add to user collection" functionality for PlantItem

--- src/components/PlantItem/PlantItem.jsx
+++ src/components/PlantItem/PlantItem.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import * as S from "./styled";
 import { useHistory } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const PlantItem = ({ title, description, img, id, temperature }) => {
   const history = useHistory();
+  const { addToUserCollection } = useAuth();
+
+  function handleOnClick() {
+    addToUserCollection(id);
+  }
+
   return (
-    <S.Container onClick={() => history.push(`/plant/${id}`)}>
+    <S.Container>
       <S.Image src={img} />
       <h4>{title}</h4>
       <p>{description}</p>
       <p>{temperature}</p>
+      <button onClick={handleOnClick}>Add to favorites</button>
+      <button onClick={() => history.push(`/plants/${id}`)}>Read more</button>
     </S.Container>
   );
 };
