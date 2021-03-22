50f6a1716c295e77ef8325c1546c20abd5bea4d5
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-22 21:31:56 +0100

    Add toggle function for fav

--- src/components/PlantItem/PlantItem.jsx
+++ src/components/PlantItem/PlantItem.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import * as S from "./styled";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+import { PlantContext } from "../../contexts/PlantContextProvider";
 
 const PlantItem = ({ title, description, img, id, temperature }) => {
+  const [heart, setHeart] = useState("♡");
   const history = useHistory();
   const { addToUserCollection, removeFromUserCollection } = useAuth();
+  const { favoritesListData } = useContext(PlantContext);
 
-  function handleOnClick() {
-    addToUserCollection(id);
-  }
+  const isInFavorites = favoritesListData.includes(id);
 
-  function removeFromFavorites() {
-    removeFromUserCollection(id);
+  useEffect(() => {
+    if (isInFavorites) {
+      setHeart("❤");
+    } else {
+      setHeart("♡");
+    }
+  }, [isInFavorites]);
+
+  function toggleFavorite() {
+    if (isInFavorites) {
+      removeFromUserCollection(id);
+    } else {
+      addToUserCollection(id);
+    }
   }
 
   return (
@@ -21,9 +34,8 @@ const PlantItem = ({ title, description, img, id, temperature }) => {
       <h4>{title}</h4>
       <p>{description}</p>
       <p>{temperature}</p>
-      <button onClick={handleOnClick}>Add to favorites</button>
-      <button onClick={removeFromFavorites}>Remove from favorites</button>
       <button onClick={() => history.push(`/plants/${id}`)}>Read more</button>
+      <button onClick={toggleFavorite}>{heart}</button>
     </S.Container>
   );
 };
