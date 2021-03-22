70bcd86a53062ff8f0e26481c6bbe416f1cc488e
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-22 22:06:41 +0100

    Create ExplorePage

---
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const ExplorePage = () => {
+  return (
+    <S.Container>
+      <h1>ExplorePage</h1>
+    </S.Container>
+  );
+};
+
+export default ExplorePage;
