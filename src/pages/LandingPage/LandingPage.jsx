b247864fee324adddda311e0667b7e72d329d91d
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-24 11:18:32 +0100

    Add styles and image

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import * as S from "./styled";
-import LoginForm from "../../components/LoginForm";
-import SignupForm from "../../components/SignupForm";
+import landing from "../../images/landing.svg";
+import Button from "../../components/Button";
+import { Link, useHistory } from "react-router-dom";
 
 const LandingPage = () => {
+  const history = useHistory();
   return (
     <S.Container>
-      <h2>LandingPage</h2>
-      <SignupForm />
-      {/* <LoginForm /> */}
+      <S.WelcomeContainer>
+        <h1>Welcome</h1>
+        <h1>keep your plants alive</h1>
+        <p>
+          Don’t forget to take care of your plants. Lets grow your lovely plant
+          and manage your entire garden.
+        </p>
+        <Button onClick={() => history.push("/signup")} title="Sign up" />
+        <Link to="/login">Already have an account? Login here</Link>
+      </S.WelcomeContainer>
+      <img src={landing} alt="landing" />
     </S.Container>
   );
 };
--- src/pages/LandingPage/styled.js
+++ src/pages/LandingPage/styled.js
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  /* display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center; */
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const WelcomeContainer = styled.div`
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+`;
