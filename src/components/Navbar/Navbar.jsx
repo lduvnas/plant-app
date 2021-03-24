a0b65524085241ffed042ac53bfe53c5afe3043e
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-03-24 14:14:50 +0100

    Create Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const Navbar = () => {
+  return (
+    <S.Container>
+      <h1>Navbar</h1>
+    </S.Container>
+  );
+};
+
+export default Navbar;
