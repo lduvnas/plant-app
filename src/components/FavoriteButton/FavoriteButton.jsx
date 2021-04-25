a4056f5b3764003e52bcdee8df1c953bdf9afa00
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-04-25 22:50:53 +0200

    Add FavoriteButton

---
+++ src/components/FavoriteButton/FavoriteButton.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { PlantContext } from "../../contexts/PlantContextProvider";
+import * as S from "./styled";
+import heart from "../../assets/svg/heart.svg";
+import heartFilled from "../../assets/svg/heartFilled.svg";
+
+const FavoriteButton = ({ plantId }) => {
+  const [favorite, setFavorite] = useState(false);
+  const { favoritesListData } = useContext(PlantContext);
+  const { addToUserCollection, removeFromUserCollection } = useAuth();
+
+  const isInFavorites = favoritesListData.includes(plantId);
+
+  useEffect(() => {
+    if (isInFavorites) {
+      setFavorite(true);
+    } else {
+      setFavorite(false);
+    }
+  }, [isInFavorites]);
+
+  function toggleFavorite() {
+    if (isInFavorites) {
+      removeFromUserCollection(plantId);
+    } else {
+      addToUserCollection(plantId);
+    }
+  }
+
+  console.log("favorite: ", favorite);
+  console.log("isInFavorites: ", isInFavorites);
+
+  return (
+    <S.HeartButton onClick={toggleFavorite}>
+      {favorite ? (
+        <S.HeartIcon src={heartFilled} alt="HeartFilled" />
+      ) : (
+        <S.HeartIcon src={heart} alt="heart" />
+      )}
+    </S.HeartButton>
+  );
+};
+
+export default FavoriteButton;
---
+++ src/components/FavoriteButton/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeartButton = styled.button`
+  position: absolute;
+  text-decoration: none;
+  background-color: transparent;
+  border: none;
+  margin: 20px;
+`;
+
+export const HeartIcon = styled.img``;
