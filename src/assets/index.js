60d91413582a0e0ff06b77ec8c86a2549d776223
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-04-25 22:50:21 +0200

    Add icons

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+import Calender from "./svg/calender.svg";
+import Sun from "./svg/sun.svg";
+import Temperature from "./svg/temperature.svg";
+import Water from "./svg/water.svg";
+import Avatar from "./svg/avatar.svg";
+import Landing from "./svg/landing.svg";
+
+export const Icons = {
+  Calender,
+  Sun,
+  Temperature,
+  Water,
+  Avatar,
+};
+
+export const Images = {
+  Landing,
+};
