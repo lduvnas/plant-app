848440017c910066ebe9bdd2baa62c26b5025ea5
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-04-26 23:07:23 +0200

    Fix styles and implemented PlantSearchDetail in ExplorePage

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -5,6 +5,7 @@ import PlantItem from "../../components/PlantItem/PlantItem";
 import Input from "../../components/Input";
 import Navbar from "../../components/Navbar";
 import { Link } from "react-router-dom";
+import PlantSearchDetail from "../../components/PlantSearchDetail/PlantSearchDetail";
 
 const ExplorePage = () => {
   const { plantListData } = useContext(PlantContext);
@@ -15,7 +16,12 @@ const ExplorePage = () => {
       <Navbar />
 
       <S.Wrapper>
-        <h1>ExplorePage</h1>
+        <S.Title>Lorem ipsum dolor sit amet eam quas malorum accusam</S.Title>
+        <S.SubTitle>
+          Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+          no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+          molestiae te.
+        </S.SubTitle>
         <Link to="/">Go back</Link>
         <Input
           type="text"
@@ -24,37 +30,40 @@ const ExplorePage = () => {
             setSearchTerm(event.target.value);
           }}
         />
-        <S.PlantListContainer>
-          {plantListData &&
-            plantListData
-              .filter((plant) => {
-                if (searchTerm === "") {
-                  return plant;
-                } else if (
-                  plant.title
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase()) ||
-                  plant.description
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase())
-                ) {
-                  return plant;
-                }
-              })
-              .map((plant) => {
-                return (
-                  <PlantItem
-                    key={plant.id}
-                    id={plant.id}
-                    title={plant.title}
-                    description={plant.description}
-                    img={plant.imageURL}
-                    temperature={plant.temperature}
-                  />
-                );
-              })}
-        </S.PlantListContainer>
+        {/* <S.FilterCard>Easy</S.FilterCard>
+        <S.FilterCard>Hard</S.FilterCard>
+        <S.FilterCard>Medium</S.FilterCard> */}
       </S.Wrapper>
+      <S.PlantListContainer>
+        {plantListData &&
+          plantListData
+            .filter((plant) => {
+              if (searchTerm === "") {
+                return plant;
+              } else if (
+                plant.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                plant.description
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase())
+              ) {
+                return plant;
+              }
+            })
+            .map((plant) => {
+              return (
+                <PlantSearchDetail
+                  key={plant.id}
+                  id={plant.id}
+                  title={plant.title}
+                  description={plant.description}
+                  img={plant.imageURL}
+                  temperature={plant.temperature}
+                  light={plant.light}
+                  careLevel={plant.careLevel}
+                />
+              );
+            })}
+      </S.PlantListContainer>
     </S.Container>
   );
 };
--- src/pages/ExplorePage/styled.js
+++ src/pages/ExplorePage/styled.js
@@ -8,10 +8,42 @@ export const Container = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  width: 60%;
+  width: 50%;
+  margin-bottom: 2em;
+`;
+
+export const Title = styled.h1`
+  font-size: 44px;
+  line-height: 56px;
+  text-align: center;
+  letter-spacing: 0.2px;
+  margin-bottom: 0.5em;
+`;
+
+export const SubTitle = styled.h2`
+  color: #737b7d;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: 0.3px;
+  font-weight: 400;
 `;
 
 export const PlantListContainer = styled.div`
+  display: grid;
+  grid-gap: 3em;
+  grid-template-columns: repeat(3, 1fr);
+  /* justify-content: space-between; */
+  /* width: 800px; */
+`;
+
+export const FilterCard = styled.div`
+  background-color: #dbedea;
+  border-radius: 10px;
+  padding: 5px 10px;
   display: flex;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: center;
+  max-width: 60px;
+  font-size: 10px;
 `;
