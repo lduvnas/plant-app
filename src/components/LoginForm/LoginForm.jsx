e3e719907a4d9db2bcc5d1c81e37eb9e96ff5994
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2021-05-05 23:51:30 +0200

    Start style login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,11 +4,16 @@ import { useAuth } from "../../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import Button from "../Button";
 import Input from "../Input";
+// import gardening from "../../assets/svg/gardening.svg";
+// import login from "../../assets/svg/login.svg";
+// import login from "../../assets/svg/login.svg";
+import welcomeLogin from "../../assets/svg/welcomeLogin.svg";
+import loginImage from "../../assets/svg/loginImage.svg";
 
 const LoginForm = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { login, signupWithGoogle } = useAuth();
+  const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
@@ -20,51 +25,44 @@ const LoginForm = () => {
       setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+      history.push("/home");
     } catch {
       setError("Failed to sign in");
     }
     setLoading(false);
   };
 
-  // const handleOnSubmit = async (e) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     setError("");
-  //     setLoading(true);
-  //     await signupWithGoogle();
-  //     history.push("/");
-  //   } catch {
-  //     setError("Failed to sign in");
-  //   }
-  //   setLoading(false);
-  // };
-
   return (
     <S.Container>
-      <h1>Login</h1>
-      {error && <p>{error}</p>}
-      <S.Form onSubmit={handleSubmit}>
-        <Input
-          type="email"
-          placeholder="email"
-          refs={emailRef}
-          required="required"
-        />
-        <Input
-          type="password"
-          placeholder="password"
-          refs={passwordRef}
-          required
-        />
-        <Button disabled={loading} type="submit" title="Login" />
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </S.Form>
-      {/* <S.Form>
-        <h3>Google login</h3>
-        <Button disabled={loading} onClick={handleOnSubmit} title="Sign Up" />
-      </S.Form> */}
+      <S.ContainerLeft>
+        {/* <h1>Welcome Back</h1>
+        <h2>To log in please enter your email and password</h2> */}
+
+        {error && <p>{error}</p>}
+        <S.Form onSubmit={handleSubmit}>
+          <S.WelcomeTitle src={welcomeLogin} alt="welcomeLogin" />
+          <Input
+            type="email"
+            placeholder="email"
+            refs={emailRef}
+            required="required"
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            refs={passwordRef}
+            required
+          />
+          <Button disabled={loading} type="submit" title="Login" />
+          Need an account? <Link to="/signup">Sign Up</Link>
+        </S.Form>
+      </S.ContainerLeft>
+
+      <S.ContainerRight>
+        <S.Image src={loginImage} alt="login" />
+      </S.ContainerRight>
+
+      {/* <S.Image src={gardening} alt="gardening" /> */}
     </S.Container>
   );
 };
--- src/components/LoginForm/styled.js
+++ src/components/LoginForm/styled.js
@@ -3,13 +3,55 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: column;
   align-items: center;
+  height: 100vh;
+`;
+
+export const ContainerLeft = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  /* background-color: #f7f7f7; */
+  /* border-bottom-right-radius: 50px;
+  border-top-right-radius: 50px; */
+  /* z-index: 2; */
+`;
+
+export const ContainerRight = styled.div`
+  /* width: 60%; */
+  height: 100%;
+  /* background-color: #8c9896; */
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  overflow: hidden;
+  /* z-index: 1; */
+  /* border-bottom-left-radius: 20px; */
+  /* border-top-left-radius: 20px; */
+  /* position: relative; */
+  /* left: -45px;
+  margin-right: -45px; */
 `;
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
   /* max-width: 400px; */
-  width: 400px;
+  width: 450px;
+  /* background-color: #fff;
+  padding: 50px;
+  border-radius: 12px;
+  box-shadow: 0 0 8px 8px #00000003; */
+`;
+
+export const Image = styled.img`
+  height: 102vh;
+`;
+
+export const WelcomeTitle = styled.img`
+  width: 300px;
+  margin-bottom: 2em;
 `;
